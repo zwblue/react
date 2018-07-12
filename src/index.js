c08558851f1c36de7c5b9d12f6ee2c9fc00df6e7
zw <18755489161@163.com>
2018-07-12 14:04:30 +0800

    npm install redux --save mobie 地址 https://antd-mobile.gitee.io/components/list-cn/

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// 新建store
+import { createStore } from 'redux'
+// 通过reducer简历
+// 根据老的state和action 生成新的state
+function counter(state = 0, action) {
+    switch (action.type) {
+        case '加机关枪':
+            return state + 1;
+        case '减机关枪':
+            return state - 1;
+        default:
+            return 10
+    }
+}
+const store = createStore(counter);
+const init = store.getState();
+console.log(init)
