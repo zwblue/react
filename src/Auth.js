62dc009d696a003a80c6af9be1aa13df2cdeebc2
zw <18755489161@163.com>
2018-07-13 17:32:10 +0800

    map循环

--- src/Auth.js
+++ src/Auth.js
@@ -12,6 +12,12 @@ import { login, getUserData } from './Auth.redux'
     }
 )
 class Auth extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            solders: ['一二三四五', '山上有老虎', '老虎不吃人',]
+        }
+    }
     componentDidMount() {
         this.props.getUserData()
     }
@@ -22,8 +28,15 @@ class Auth extends React.Component {
                 <h2>我的名字是{this.props.user},年龄{this.props.age}</h2>
                 <h2>你没有权限，需要登录才能看</h2>
                 <button onClick={this.props.login}>登录</button>
-                <h3>{this.props.user}</h3>
+                <h3>1111{this.props.name}</h3>
                 <h3>是否登录：{this.props.isAuth ? '已登录' : '未登录'}</h3>
+                <div>
+                    {
+                        this.state.solders.map((val) => {
+                            return <div key={val}>{val}</div>
+                        })
+                    }
+                </div>
             </div>
         )
     }
