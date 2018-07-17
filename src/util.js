41938e5aea2f9b788ef55d0a89edf4cba39b4091
zw <18755489161@163.com>
2018-07-17 14:45:51 +0800

    定义根据接口获取信息跳转新的路由的方法

---
+++ src/util.js
@@ -0,0 +1,8 @@
+export function getRedirectPath({ type, avatar }) {
+    // 根据用户信息返回跳转地址
+    // user.type/boss /genius
+    // user.avatar /booinfo /geniusinfo
+    let url = (type === 'boss') ? '/boss' : '/genius'
+    if (!avatar) { url += 'info' }
+    return url
+}
